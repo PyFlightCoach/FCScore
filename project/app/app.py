15fa99174d40fadd1daf9d7daecab8f7e3d64f33
Thomas David <thomasdavid0@gmail.com>
2023-07-06 12:11:10 +0100

    changed to by the book scoring

--- project/app/app.py
+++ project/app/app.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dcc, Output, Input, State, dash_table, dcc
 from dash.dash_table import DataTable
 from dash.dash_table.Format import Scheme, Format
 import dash_bootstrap_components as dbc
+from dash.exceptions import PreventUpdate
 import plotly.express as px
 from json import load
 from io import StringIO
@@ -18,188 +19,202 @@ from multiprocessing import Process, Queue
 from datetime import datetime
 from time import time, sleep
 from uuid import uuid4
-from threading import Thread
-from geometry import Quaternion
+
 
 app_data = {}
 
-def cleanup():
-    ad = {k: data["last_hb"] for k, data in app_data.items()}
-    last_allowed_hb = time() - 10
-    pop_list = [k for k, v in ad.items() if last_allowed_hb > v]
-    
-    for pop_sess in pop_list:
-        print(f"removing user {pop_sess}")
-        app_data.pop(pop_sess)
-
 
 app = Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
+app.config["suppress_callback_exceptions"] = True
 server = app.server
+
+
 def serve_layout():
     session_id = str(uuid4())
 
     app_data[session_id] = dict(
         started = time(),
-        status = ["initializing"],
+        status = ["initialized"],
         last_hb = time(),
+        manoeuvre_count = 17,
         analysis = ScheduleAnalysis(),
         q = Queue(),
         p = None,
+        scores = pd.DataFrame(columns = ["name","k", "inter_dg", "intra_dg", "exit_dg", "score"]),
+        total_score = 0.0,
     )
     app_data[session_id]["status"].append([f"user connected at {datetime.now()}, session_id = {session_id}, active users = {len(app_data)}"],)
     return html.Div(
     [
         dcc.Store(data=session_id, id='session-id'),
-        dbc.Row([dbc.Col(dcc.Upload(
-                id='upload-json',
-                children=html.Div(['Drag and Drop or ',html.A('Select FC json file')]),
-                style={
-                    'width': '100%',
-                    'height': '30px',
-                    'lineHeight': '30px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    'borderRadius': '2px',
-                    'textAlign': 'center',
-                    'margin': '10px'
-                })),
-                dbc.Col(html.Div(id="status"), align="center")
-        ],justify="center"),
-        dbc.Row(
-            [
-
-                dbc.Col(
-                    [
-                        html.P("Once a json file is loaded scores should start to appear in the table below"),
-                        html.P("You can click on a row of the table to see a plot of the flown manoeuvre and the corrected template"),
-                        DataTable(
-                            id='score-table', 
-                            fill_width=False, 
-                            columns=[
-                                dict(id="name", name="name"),
-                                dict(id="k", name="k", type="numeric"),
-                                dict(id="inter_dg", name="inter_dg", type="numeric", format=Format(precision=2, scheme=Scheme.decimal)),
-                                dict(id="intra_dg", name="intra_dg", type="numeric", format=Format(precision=2, scheme=Scheme.decimal)),
-                                dict(id="exit_dg", name="exit_dg", type="numeric", format=Format(precision=2, scheme=Scheme.decimal)),
-                                dict(id="score", name="score", type="numeric", format=Format(precision=2, scheme=Scheme.decimal)),
-                            ]
-                        ), 
-                        html.H2(id="total-score")
-                    ], width=3),
-                dbc.Col(dcc.Graph(id='plot-analysis', style={'height': '90vh'}))
-            ]
-        ),
-        dcc.Interval(
-			id='interval-component',
-			interval=3000, # in milliseconds
-			n_intervals=0
-		),
-        html.P(id='placeholder') 
-    ]    
+        dcc.Interval(id="session-heartbeat", interval=5000, n_intervals=0),
+        dbc.Row([
+            html.Div(id="uploader", children=dcc.Upload(
+                    id='upload-json',
+                    children=html.Div(['Drag and Drop or ',html.A('Select FC json file')]),
+                    style=dict(width='100%',height='30px',lineHeight='30px',borderWidth='1px',borderStyle='dashed',borderRadius='2px',textAlign='center',margin='10px')
+            )),
+            html.Div(id="status"),
+            dcc.Interval(id='upload-status-interval',interval=3000, n_intervals=0, disabled=True)
+        ]),
+        dbc.Row([
+            dbc.Col([html.Div(id="score-table"), html.H2(id="total-score")], width=3),
+            dbc.Col([html.Div(id="summary-man")])
+        ]),
+        dbc.Row([html.Div(id="summary-el")])
+    ]
 )
 
 app.layout = serve_layout
 
 
-def parsefcj(data: dict, q: Queue):
-    try:
-        q.put("parsing flight data")
-        flight = Flight.from_fc_json(data)
-
-        q.put("parsing flightline")
-        box = Box.from_fcjson_parmameters(data["parameters"])
-
-        q.put("generating state information")
-        state = State.from_flight(flight, box).splitter_labels(data["mans"])
-
-        q.put(f"reading {data['parameters']['schedule'][1]} schedule definition")
-        sdef = get_schedule_definition(data["parameters"]["schedule"][1])
-        
-        for mid in range(17):
-            q.put(f"Analysing manoeuvre {mid+1}")
-            q.put(ManoeuvreAnalysis.build(sdef[mid], state.get_meid(mid+1)).to_dict())
-
-        q.put(f"Analysis Complete")
-    except Exception as ex:
-        q.put(f"Analysis Error: {ex.message}")
-    
-
 @app.callback(
-    Output('placeholder', 'children'), 
+    Output('uploader', 'children'),
+    Output('upload-status-interval', 'disabled', allow_duplicate=True),
     Input('upload-json', 'contents'),
-    Input('session-id', 'data')
+    Input('session-id', 'data'),
+    prevent_initial_call=True
 )
-def update_output(contents, session_id):
+def upload_fcjson(contents, session_id):
     ad = app_data[session_id]
-    if not ad["p"] is None:
-        ad["status"].append("Cannot Restart")
-    elif contents is not None:
-        ad["status"].append("started")
+    if contents is not None:
+
+        def parsefcj(data: dict, q: Queue):
+            try:
+                q.put("parsing flight data")
+                flight = Flight.from_fc_json(data)
+
+                q.put("parsing flightline")
+                box = Box.from_fcjson_parmameters(data["parameters"])
+
+                q.put("generating state information")
+                state = State.from_flight(flight, box).splitter_labels(data["mans"])
+
+                q.put(f"reading {data['parameters']['schedule'][1]} schedule definition")
+                sdef = get_schedule_definition(data["parameters"]["schedule"][1])
+                q.put(f"manoeuvre count = {len(sdef)}")
+                for mid in range(17):
+                    q.put(f"Analysing manoeuvre {mid+1}")
+                    q.put(ManoeuvreAnalysis.build(sdef[mid], state.get_meid(mid+1)).to_dict())
+
+                q.put(f"Analysis Complete")
+            except Exception as ex:
+                q.put(f"Analysis Error: {ex.message}")
+
         content_type, content_string = contents.split(',')
         data = load(StringIO(base64.b64decode(content_string).decode('utf-8')))
         ad["p"] = Process(target=parsefcj, args=(data, ad["q"]))
         ad["p"].start()
-    return None
+        ad["status"].append(f"Analysing {data['name']}")
+        return None, False
+    else:
+        raise PreventUpdate()
+
+
+@app.callback(
+    Output('session-heartbeat', 'disabled'),
+    Input('session-heartbeat', 'n_intervals'),
+    Input('session-id', 'data')
+)
+def user_manager(i, session_id):
+    ad = app_data[session_id]
+    ad["last_hb"] = time()
+    
+    sessions = {k: data["last_hb"] for k, data in app_data.items()}
+    last_allowed_hb = time() - 7
+    pop_list = [k for k, v in sessions.items() if last_allowed_hb > v]
+    
+    for pop_sess in pop_list:
+        print(f"removing user {pop_sess}")
+        app_data.pop(pop_sess)
+
+    return False
 
 
 @app.callback(
     Output('status', 'children'),
-    Output('score-table', 'data'),
-    Output('total-score', 'children'),
-    Input('interval-component', 'n_intervals'),
-    Input('session-id', 'data')
+    Output('upload-status-interval', 'disabled', allow_duplicate=True),
+    Input('upload-status-interval', 'n_intervals'),
+    Input('session-id', 'data'),
+    prevent_initial_call=True
 )
 def update_status(i, session_id):
     ad = app_data[session_id]
-    ad["last_hb"] = time()
     if not ad["q"].empty():
         message = ad["q"].get()
         if isinstance(message, str):
             ad["status"].append(message)
             if message == "Analysis Complete":
                 pass
+            elif "manoeuvre count =" in message:
+                ad["manoeuvre_count"] = int(message.split("=")[1])
         elif isinstance(message, dict):
             ma = ManoeuvreAnalysis.from_dict(message)
-            
-            ma.intra_dgs = ma.mdef.mps.collect(ma.intended)
-            ma.intra_dg = sum([dg.value for dg in ma.intra_dgs])
-
-            ma.inter_dgs = ma.intended.analyse(ma.aligned, ma.intended_template)
-            ma.inter_dg = ma.inter_dgs.downgrade()
-
             ad["analysis"].add(ma)
             ad["status"].append(f"Completed Analysis of {ma.mdef.info.name}")
-    cleanup()
+    
+    return ad["status"][-1], ad["status"][-1] == "Analysis Complete"
 
-    scores = []
-
-    for ma in ad["analysis"]:
-        scores.append(dict(
-            name=ma.mdef.info.short_name,
-            k=ma.mdef.info.k,
-            inter_dg = ma.inter_dg,
-            intra_dg = ma.intra_dg,
-        ))
-
-    if len(scores) > 0:
-        scores = pd.DataFrame(scores)
-        scores["score"] = np.maximum(0, 10 - scores.inter_dg - scores.intra_dg)
-    else:
-        scores = pd.DataFrame(columns = ["name","k", "inter_dg", "intra_dg", "exit_dg", "score"])
-    return ad["status"][-1], scores.to_dict('records'), sum(scores.score * scores.k)
 
 @app.callback(
-    Output('plot-analysis', 'figure'), 
-    Input('score-table', 'active_cell'),
-    Input('session-id', 'data')
+    Output('score-table', 'children'),
+    Output('total-score', 'children'),
+    Input('upload-status-interval', 'n_intervals'),
+    Input('session-id', 'data') 
+)
+def update_scores(i, session_id):
+    ad = app_data[session_id]
+    if len(ad["analysis"]) > len(ad["scores"]):
+        scores = []
+        for ma in ad["analysis"]:
+            if not hasattr(ma, "intra_dg"):
+                ma.intra_dgs = ma.intended.analyse(ma.aligned, ma.intended_template)
+                ma.intra_dg = ma.intra_dgs.downgrade()
+            if not hasattr(ma, "inter_dg"):
+                ma.inter_dgs = ma.mdef.mps.collect(ma.intended)
+                ma.inter_dg = sum([dg.value for dg in ma.inter_dgs])
+
+            scores.append(dict(
+                name=ma.mdef.info.short_name,
+                k=ma.mdef.info.k,
+                inter_dg = ma.inter_dg,
+                intra_dg = ma.intra_dg,
+                score = max(0, 10 - ma.inter_dg - ma.intra_dg)
+            ))
+        
+        ad["scores"] = pd.DataFrame(scores)
+        ad["total_score"] = f"{sum(ad['scores'].score * ad['scores'].k):.2f}"
+        df = ad["scores"].round(2)
+
+        dt =  DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], id="scoretab")
+
+        return dt, ad["total_score"]
+    else:
+        if len(ad["scores"]) == ad["manoeuvre_count"]:
+            ad["status"].append("Analysis Complete")
+        raise PreventUpdate()
+
+
+@app.callback(
+    Output('summary-man', 'children'), 
+    Input('scoretab', 'active_cell'),
+    Input('session-id', 'data'),
+    prevent_initial_call=True
 )
 def update_graphs(active_cell, session_id):
     if active_cell:
-        return app_data[session_id]["analysis"][active_cell["row"]].plot_3d(nmodels=30)
-    else:
-        return go.Figure()
+        ma = app_data[session_id]["analysis"][active_cell["row"]]
 
+        graph = dcc.Graph(id='plot-analysis', style={'height': '90vh'}, figure=ma.plot_3d(nmodels=30))
+    
+        inter_df = ma.inter_dgs.downgrade_df().round(2)
+        inter_tab = DataTable( inter_df.to_dict('records'), [{"name": i, "id": i} for i in inter_df.columns])
 
+        intra_df = ma.intra_dgs.downgrade_df().round(2).reset_index()
+        intra_tab = DataTable( intra_df.to_dict('records'), [{"name": i, "id": i} for i in intra_df.columns])
+
+        return [graph, inter_tab, intra_tab]
+    raise PreventUpdate()
 
 
 if __name__ == "__main__":
